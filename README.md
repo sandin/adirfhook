36504be3e8901c76641b18ff54b9f08dc39df7a7
lds <lds2012@gmail.com>
2022-10-26 17:44:24 +0800

    import

---
+++ README.md
@@ -0,0 +1,11 @@
+# adirfhook
+
+An android hook library base on frida.
+
+## Build
+
+```
+$ mkdir build && cd build
+$ export FRIDA_BUILD_PATH=/your_frida/build
+$ export ANDROID_NDK_ROOT=/your_android_ndk
+$ ../build.sh
---
+++ build.sh
@@ -0,0 +1,15 @@
+#ABI=armeabi-v7a
+ABI=arm64-v8a
+
+cmake \
+  -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake \
+  -DANDROID_ABI=$ABI \
+  -DANDROID_NATIVE_API_LEVEL=19 \
+  -DCMAKE_MAKE_PROGRAM=make \
+  -DANDROID_STL=c++_static \
+  -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=. \
+  -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=. \
+  ..
+
+#-DCMAKE_BUILD_TYPE=Release \
+cmake --build .
